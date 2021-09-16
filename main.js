d624e4f369e3fe0185f5f2741e23e1fe199d1cc9
nggadev <nggadev@gmail.com>
2021-09-16 10:12:45 +0700

    update

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 // get Element
-const user_place = document.getElementById('user');
+const cart_items = document.getElementById('cart');
 
 // ES6
 
 
 // ES6
-const user_logged = "Robert";
-user_place.innerHTML = `Hi ${user_logged}`;
-console.log(user_logged);
+function addProduct(name, category) {
+    return {
+        name,
+        category
+    }
+}
+
+var getProduct = addProduct("iPhone 11", "Gadget");
+cart_items.innerHTML = `Product: ${getProduct.name} Category: ${getProduct.category}`;
