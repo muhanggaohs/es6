808b1f38b9aca46cd4971df767fc05279401d206
nggadev <nggadev@gmail.com>
2021-09-16 09:32:45 +0700

    update

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // ES6
-var myName = "Angga";
 
-myName = "Oktaharisetia"
-myName = "Dev";
-// console.log(myName);
+
 
 // ES6
-let myCar = "Civic";
-myCar = "Xpander"
-console.log(myCar);
+
+let myCars = ['BMW', 'Honda', 'Mitsubishi'];
+
+for (let i = 0; i < myCars.length; i++) {
+    console.log(myCars[i]);
+}
