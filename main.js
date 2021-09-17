91ffd6852709c8f15d1b8c94b605745149e0f296
nggadev <nggadev@gmail.com>
2021-09-17 11:03:29 +0700

    update

--- main.js
+++ main.js
@@ -16,7 +16,25 @@ class Siswa {
         console.log(this.username + ' Joined ' + this.nama_kelas);
     }
 
+    countSiswa() {
+        console.log('Available 100 Siswa');
+    }
+
 }
 
-let addSiswa = new Siswa('Angga', '123123', 'OOP');
-addSiswa.join();
+// let addSiswa = new Siswa('Angga', '123123', 'OOP');
+// addSiswa.join();
+
+class Subs extends Siswa {
+    constructor(username, paket) {
+        super(username);
+        this.paket = paket;
+    }
+
+    paketSubs() {
+        console.log('Hi ' + this.username + ' subs paket ' + this.paket);
+    }
+}
+
+let addSubs = new Subs('Angga', 'Premium');
+addSubs.countSiswa();
